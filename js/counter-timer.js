20c69c125f1a8dbe92ab62550c26d8d78d59f14f
Shalin Chopra <schopr10@hawk.iit.edu>
2022-04-14 22:41:00 -0400

    updating timer code with india timings

--- js/counter-timer.js
+++ js/counter-timer.js
@@ -1,21 +1,21 @@
-var chi = setInterval(function() { makeTimerChicago(); }, 1000);
+var timerInd = setInterval(function() { createTimer(); }, 1000);
 
 
 
-function makeTimerChicago() {
+function createTimer() {
 
-    var endTime = new Date("April 15, 2022 00:00:00 CDT");	
-    var endTime = (Date.parse(endTime)) / 1000;
+    let endTime = new Date("April 16, 2022 00:00:00 GMT+5:30");
+    endTime = (Date.parse(endTime)) / 1000;
 
-    var now = new Date();
-    var now = (Date.parse(now) / 1000);
+    let now = new Date();
+    now = (Date.parse(now) / 1000);
 
-    var timeLeft = endTime - now;
+    let timeLeft = endTime - now;
 
-    var days = Math.floor(timeLeft / 86400); 
-    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+    let days = Math.floor(timeLeft / 86400);
+    let hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    let minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+    let seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
     if (hours < "10") { hours = "0" + hours; }
     if (minutes < "10") { minutes = "0" + minutes; }
@@ -31,9 +31,6 @@ function makeTimerChicago() {
         document.getElementById("india").innerHTML = "Wishing you a Very Very Happpyyy B'day Kidddooo ...";
         document.getElementById("btnNew").style.visibility = "visible";
         
-        clearInterval(chi);
+        clearInterval(timerInd);
     }
-
 }
-
-
